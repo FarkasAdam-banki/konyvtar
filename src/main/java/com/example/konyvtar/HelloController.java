fd27a65bce80e42a137389e08e68c8b990dc8a8a
Kalauz.Balazs <77264674+KalauzBalazs@users.noreply.github.com>
2025-01-23 11:31:28 +0100

    kölcsönzés felvétele hozzáadása a menühöz

--- src/main/java/com/example/konyvtar/HelloController.java
+++ src/main/java/com/example/konyvtar/HelloController.java
@@ -1,14 +1,23 @@
 package com.example.konyvtar;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private BorderPane bp;
+    public void showKolcsonzesFelvetele() {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("kolcsonzes-felvetele.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        bp.setCenter(root);
     }
 }
