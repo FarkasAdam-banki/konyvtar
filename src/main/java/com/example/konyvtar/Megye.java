2b19d4895ce41a17bd16921ab74954ae01881e24
Krisztian0801 <bordacs.krisztian.2020i@bankitatabanya.hu>
2025-01-26 17:27:52 +0100

    member-registration-v0.1

---
+++ src/main/java/com/example/konyvtar/Megye.java
@@ -0,0 +1,33 @@
+package com.example.konyvtar;
+
+public class Megye {
+
+    private int megye_id;
+    private String megye_megnevezes;
+
+    public Megye(int megye_id, String megye_megnevezes) {
+       setMegye_id(megye_id);
+       setMegye_megnevezes(megye_megnevezes);
+    }
+
+    public int getMegye_id() {
+        return megye_id;
+    }
+
+    public void setMegye_id(int megye_id) {
+        this.megye_id = megye_id;
+    }
+
+    public String getMegye_megnevezes() {
+        return megye_megnevezes;
+    }
+
+    public void setMegye_megnevezes(String megye_megnevezes) {
+        this.megye_megnevezes = megye_megnevezes;
+    }
+
+    @Override
+    public String toString() {
+        return this.getMegye_megnevezes();
+    }
+}
