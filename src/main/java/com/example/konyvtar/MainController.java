fab3719257f8df0841afe97e7dd1482ad4346bfb
SGabor <77265814+SGaborS@users.noreply.github.com>
2025-01-27 09:12:05 +0100

    Optimalizálás

---
+++ src/main/java/com/example/konyvtar/MainController.java
@@ -0,0 +1,41 @@
+package com.example.konyvtar;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+
+public class MainController {
+    @FXML
+    private BorderPane bp;
+    public void showKolcsonzesFelvetele() {
+        setFXMLContents("kolcsonzes-felvetele.fxml");
+    }
+    public void showKolcsonzesekListaja() {
+        setFXMLContents("kolcsonzesek-listaja.fxml");
+    }
+    public void showKonyvFelvetel() {
+        setFXMLContents("konyv-felvetel.fxml");
+    }
+    public void showKonyvekKeresese() {
+        setFXMLContents("konyvek-keresese.fxml");
+    }
+    public void showTagRegisztralasa() {
+        setFXMLContents("tag-regisztralasa.fxml");
+    }
+    public void showTagokListaja() {
+        setFXMLContents("tagok-listaja.fxml");
+    }
+
+    public void setFXMLContents(String fileName) {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource(fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        bp.setCenter(root);
+    }
+}
